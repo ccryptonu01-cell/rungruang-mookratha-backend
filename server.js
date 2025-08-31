977784dfdd2eb1ff470d101c12046d88132d2f66
Anubiz999 <anuchit.nu@gmail.com>
2025-09-01 00:57:08 +0700

    e

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const FRONTEND_DEV = "http://localhost:5173"; // React dev server
 const FRONTEND_PROD = [
     "https://rungruang-mookratha-frontend.vercel.app",
     "https://rungruang-mookratha-frontend-git-main-rrmks-projects.vercel.app",
-    "https://rungruang-mookratha-frontend-2giw7cw51-rrmks-projects.vercel.app"
+    "https://rungruang-mookratha-frontend-dtl8cim3x-rrmks-projects.vercel.app"
 ];
 const API_DEV = `http://localhost:${port}`;
 const CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "dybgekx5y";
