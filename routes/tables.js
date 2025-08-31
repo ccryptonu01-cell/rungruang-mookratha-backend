47f1dd969af5550b7dd5a46e8f4dbf8c9332b732
Anubiz999 <anuchit.nu@gmail.com>
2025-09-01 01:54:23 +0700

    e

--- routes/tables.js
+++ routes/tables.js
@@ -3,8 +3,7 @@ const { getTables, getTableByNumber } = require('../controllers/tables')
 const { verifyToken, verifyAdmin, verifyCashier } = require('../middleware/authMiddleware')
 const router = express.Router()
 
-router.get('/guest/tables', getTables)
-router.get('/user/tables', verifyToken, getTables)
+router.get('/tables', getTables)
 router.get("/tables/:number", verifyToken, getTableByNumber);
 
 module.exports = router;
